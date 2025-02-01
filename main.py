124b6944dd5aa27f16277ab70c478879fc39bb6a
Anirban Basu <anirbanbasu@Anirbans-MacBook-Air.local>
2025-02-01 20:58:51 +0530

    Creating New Flash Cards

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
 # Putting the card front image into the window
 from tkinter import* 
+import pandas as pd, random
 BACKGROUND_COLOR = "#B1DDC6"
+
+df = pd.read_csv("french_words.csv")
+# converts dataframe into a list of dictionaries (refer test.py)
+french2eng = df.to_dict(orient="records")
+def next_card():
+
+    # This inputs a dictionary element from the list of dictionaries
+    current_card = random.choice(french2eng)
+    canvas.itemconfig(card_title, text= "French")
+    canvas.itemconfig(card_word, text=current_card["French"])
 
 window = Tk()
 window.title("Flashy")
@@ -11,13 +22,22 @@ card_front_img = PhotoImage(file="card_front.png")
 
 # Putting the image at center point, to fill uniformly
 canvas.create_image(400,263, image= card_front_img)
-canvas.create_text(400,150, text="Title", font=("Ariel",40,"italic"),fill="black")
-canvas.create_text(400,263, text="word", font=("Ariel",60,"bold"),fill = "black")
+card_title = canvas.create_text(400,150, text="Title", font=("Ariel",40,"italic"),fill="black")
+card_word = canvas.create_text(400,263, text="word", font=("Ariel",60,"bold"),fill = "black")
 
 # Changing bgcolor of image to avoid discoloured thickness 
 canvas.config(bg=BACKGROUND_COLOR, highlightthickness = 0)
-canvas.grid(row=0, column=0)
+# columnspan=2, So that the upperpart is seperated and below part can be put to grid properly
+canvas.grid(row=0, column=0, columnspan=2)
 
+wrong_image = PhotoImage(file= "wrong.png")
+wrong_button = Button(image= wrong_image, highlightthickness=0, command= next_card)
+wrong_button.grid(row=1, column=0)
 
+right_image = PhotoImage(file= "right.png")
+right_button = Button(image= right_image, highlightthickness=0, command= next_card)
+right_button.grid(row=1, column=1)
+
+next_card()
 
 window.mainloop()
--- test.py
+++ test.py
@@ -1,8 +1,18 @@
-import pandas as pd
+import pandas as pd, random
 
 def random_words():
     df = pd.read_csv('french_words.csv')
-    french2eng = df.set_index('French')['English'].to_dict()
+    # french2eng = df.set_index('French')['English'].to_dict()
+
+
+    # This code converts a Pandas DataFrame (df) into a list of dictionaries, where each dictionary represents a row in the DataFrame.
+    french2eng = df.to_dict(orient="records")
+
+    current_card = random.choice(french2eng)
+
     print(french2eng)
+    print("\n\n")
+    print(current_card)
+
 
 random_words()
