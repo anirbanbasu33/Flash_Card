4fb961171b2f2db4bfe51f5e2d7c2d13312a9bf6
Anirban Basu <anirbanbasu@Anirbans-MacBook-Air.local>
2025-02-01 21:27:54 +0530

    Flipping Cards

--- main.py
+++ main.py
@@ -6,22 +6,40 @@ BACKGROUND_COLOR = "#B1DDC6"
 df = pd.read_csv("french_words.csv")
 # converts dataframe into a list of dictionaries (refer test.py)
 french2eng = df.to_dict(orient="records")
-def next_card():
 
-    # This inputs a dictionary element from the list of dictionaries
+current_card = {}
+
+def next_card():
+    # Making it global to acceess this outside next_card()
+    global current_card, flip_timer
+    # Every time we get inside a new card, we will invalidate the flip_timer
+    window.after_cancel(flip_timer)
     current_card = random.choice(french2eng)
-    canvas.itemconfig(card_title, text= "French")
-    canvas.itemconfig(card_word, text=current_card["French"])
+    canvas.itemconfig(card_title, text= "French", fill="black")
+    canvas.itemconfig(card_word, text=current_card["French"], fill="black")
+    canvas.itemconfig(card_background, image= card_front_img)
+    # Flip card after 3 seconds
+    flip_timer = window.after(3000, func= flip_card)
+
+def flip_card():
+    canvas.itemconfig(card_title, text= "English", fill= "white")
+    canvas.itemconfig(card_word, text= current_card["English"], fill= "white")
+    canvas.itemconfig(card_background, image= card_back_img)
+
 
 window = Tk()
 window.title("Flashy")
 window.config(padx= 50, pady= 50, bg= BACKGROUND_COLOR)
+# Flip card after 3 seconds
+flip_timer = window.after(3000, func= flip_card)
+
 
 canvas = Canvas(width=800, height=526)
-card_front_img = PhotoImage(file="card_front.png")
+card_front_img = PhotoImage(file= "card_front.png")
+card_back_img = PhotoImage(file= "card_back.png")
 
 # Putting the image at center point, to fill uniformly
-canvas.create_image(400,263, image= card_front_img)
+card_background = canvas.create_image(400,263, image= card_front_img)
 card_title = canvas.create_text(400,150, text="Title", font=("Ariel",40,"italic"),fill="black")
 card_word = canvas.create_text(400,263, text="word", font=("Ariel",60,"bold"),fill = "black")
 
