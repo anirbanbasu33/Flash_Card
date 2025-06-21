f7b7000559a92082467cf0dabc55e2cc86bd50c6
Anirban Basu <anirbanbasu33@gmail.com>
2025-06-21 10:02:52 +0530

    remove spaces
    nothing to tell

--- test.py
+++ test.py
@@ -4,7 +4,6 @@ def random_words():
     df = pd.read_csv('french_words.csv')
     # french2eng = df.set_index('French')['English'].to_dict()
 
-
     # This code converts a Pandas DataFrame (df) into a list of dictionaries, where each dictionary represents a row in the DataFrame.
     french2eng = df.to_dict(orient="records")
 
@@ -13,6 +12,5 @@ def random_words():
     print(french2eng)
     print("\n\n")
     print(current_card)
-
 
 random_words()
